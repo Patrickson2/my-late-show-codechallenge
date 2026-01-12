23b5d6c9851f8fdfdcbcdd8668a95e6443b68cd1
Patrickson2 <thairupatrickson@gmail.com>
2026-01-13 00:20:45 +0300

    Generated a database config

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from models import *
 
 app = Flask(__name__)
 
+#created the database configuration.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
