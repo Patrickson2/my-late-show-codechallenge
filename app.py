2c1d38c50378baf2f9f88925b245e537b0896e8b
Patrickson2 <thairupatrickson@gmail.com>
2026-01-13 00:23:34 +0300

    initialised the database with the flask app

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from models import *
 
+#creating a constructor for this flask class application
 app = Flask(__name__)
 
 #created the database configuration.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#initializing the database with the flask app
+db.init_app(app)
+
+
 
 
 
