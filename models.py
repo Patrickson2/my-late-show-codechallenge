ad2e5491028142c8ee4bd0e389e5a942294b7af5
Patrickson2 <thairupatrickson@gmail.com>
2026-01-13 10:31:25 +0300

    created all classes for this application

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+fro
 
 
-
+db = SQLAlchemy()
 
 class Episode(db.Model):
     __tablename__ = 'episodes'
@@ -9,3 +10,17 @@ class Episode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(10))
     number = db.Column(db.Integer)
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    occupation = db.Column(db.String(100))
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+         
